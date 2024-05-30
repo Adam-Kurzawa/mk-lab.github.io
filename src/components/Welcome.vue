0a51cb362810b5f3b1e2fd44f001692d57e59a25
Adam Kurzawa <adam.kurzawa.70@gmail.com>
2024-05-30 17:06:58 +0200

    Phone

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -10,10 +10,13 @@ const t = useTranslation();
     <div class="left">
       <h1>{{ t('welcome.h1') }}</h1>
       <h2>{{ t('welcome.h2') }}</h2>
-      <div class="hours">Pon. - Pt. 07:00 - 11:00</div>
+      <div class="info">
+        <span class="hours">Pon. - Pt. 07:00 - 11:00</span>
+        <span class="hours">Telefon 669 516 349</span>
+      </div>
       <div class="left-description font-segoe">{{ t('welcome.desc') }}</div>
-      <img class="left-map" src="@/assets/maps.jpg" />
       <a class="left-button font-segoe" href="https://login-my.synevo.pl/mysynevo/login.do?client_id=5PP6XQnFvuxZ2q9mha2MTLY9zAka&code_challenge=GI5AuFYstV_JVaDaOoyj0ytCfIO2nqWpeJKpNaezj6U&code_challenge_method=S256&commonAuthCallerPath=%2Foauth2%2Fauthorize&forceAuth=false&passiveAuth=false&redirect_uri=https%3A%2F%2Fmy.synevo.pl&response_mode=query&response_type=code&scope=openid+profile&state=3880f23462624de0ac837aacab840da5&tenantDomain=carbon.super&sessionDataKey=b5bee604-31bc-4a01-9b36-5f11e4981d9b&relyingParty=5PP6XQnFvuxZ2q9mha2MTLY9zAka&type=oidc&sp=my-synevo&isSaaSApp=false&authenticators=BasicAuthenticator%3ALOCAL&reCaptcha=true">{{ t('welcome.button') }}</a>
+      <img class="left-map" src="@/assets/maps.jpg" />
     </div>
     <img class="right" src="@/assets/blood.png" />
   </Panel>
@@ -40,6 +43,13 @@ const t = useTranslation();
   gap: 2rem;
 }
 
+.info {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  gap: 2rem;
+  padding-right: 2rem;
+}
+
 .left {
   text-align: left;
 }
@@ -54,9 +64,9 @@ const t = useTranslation();
 }
 
 .left-map {
-  margin-bottom: 2rem;
   width: 100%;
   border-radius: 2rem;
+  margin-top: 2rem;
 }
 
 .left-button {
