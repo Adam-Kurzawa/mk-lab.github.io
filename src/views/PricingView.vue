d9589fb978f09b5cd54c0c7ef4b8e3e0d3b695e8
Adam Kurzawa <adam.kurzawa.70@gmail.com>
2024-06-08 22:28:29 +0200

    Sxx

--- src/views/PricingView.vue
+++ src/views/PricingView.vue
@@ -16,6 +16,8 @@ const subLevelEntry = (entry) => Object.entries(entry)
   <main class="container">
     <input type="text" v-model="searchPhrase" placeholder="Wyszukaj badanie..."/>
     <h1>{{ t('pricing.h1') }}</h1>
+    <div class="font-josefin simple-text">Badanie na cito płatne dodatkowo 2 zł.</div>
+    <div class="font-josefin simple-text">Usługa szczepień osób dorosłych płatna 20 zł.</div>
     <span v-if="pricing" v-for="values in pricingKeyValues">
       <h2>{{ values[0] }}</h2>
       <Pricing v-for="subValues in subLevelEntry(values[1])" :title="subValues[0]" :pricing="subValues[1]" :filter="searchPhrase" />
@@ -38,6 +40,13 @@ input {
   outline: none;
   font-size: 1.25rem;
   color: #5463a9;
+}
+
+.simple-text {
+  margin-right: 1rem;
+  margin-left: 1rem;
+  margin-bottom: 1rem;
+  font-size: 1.15rem;
 }
 
 input, h1, h2 {
